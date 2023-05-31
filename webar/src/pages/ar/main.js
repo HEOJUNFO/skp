ace49b75a4ed5dcfef2b8e7d1434609a9acc7087
heojunfo <kleague3@gmail.com>
2023-05-31 17:09:14 +0900

    ar main.js comment revert

--- webar/src/pages/ar/main.js
+++ webar/src/pages/ar/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import store from '@/store'
 
 // html에서 import
+// import 'aframe'
+
 import 'mind-ar/dist/mindar-face.prod.js';
 import 'aframe-extras'
 import 'mind-ar/dist/mindar-face-aframe.prod.js';
