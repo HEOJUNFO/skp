71c85e1dc998301d121cf5ee4e05c3f806688e1d
heojunfo <hjun0410@naver.com>
2023-06-04 21:58:23 +0900

    basic cleanup

--- webar/src/pages/basic/router/index.js
+++ webar/src/pages/basic/router/index.js
@@ -2,16 +2,13 @@ import {createRouter/*, createWebHistory*/, createWebHashHistory} from "vue-rout
 
 import Home from "@/pages/basic/views/Home";
 import Basic from "@/pages/basic/views/Basic";
-import Frame from "@/pages/basic/views/FrameBasic"
 import DragNDrop from "@/pages/basic/views/DragNDrop";
-import Face from "@/pages/basic/views/FaceBasic";
+
 
 export const routes = [
     {path: "/", component: Home, name: "Home"},
     {path: "/basic", component: Basic, name: "Basic"},
-    {path: "/frame", component: Frame, name: "Frame"},
     {path: "/drag-n-drop", component: DragNDrop, name: "DragNDrop"},
-    {path: "/Face", component: Face, name: "Face"},
 ]
 
 const router = createRouter({
