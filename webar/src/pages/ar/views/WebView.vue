e1fafb804ef7c570b0d46d25bf7cceb453a1d765
heojunfo <kleague3@gmail.com>
2023-06-13 16:19:01 +0900

    refactor gaussianBlur

--- webar/src/pages/ar/views/WebView.vue
+++ webar/src/pages/ar/views/WebView.vue
@@ -29,6 +29,7 @@ import useResultData from "@/composables/useResultData";
 import useLoading from "@/composables/useLoading";
 import useEventHandlers from "@/composables/useEventHandlers";
 import useWindowEvent from "@/composables/useWindowEvent";
+import gaussianBlur from "@/js/gaussianBlur";
 
 export default {
   name: "WebView",
@@ -152,39 +153,6 @@ export default {
       } else {
         containerRef.value.topValue = 40;
       }
-    }
-
-    function gaussianBlur(context, width, height, radius) {
-      const imageData = context.getImageData(0, 0, width, height);
-      const data = imageData.data;
-      const pixels = new Uint8Array(data.length);
-
-      for (let y = 0; y < height; y++) {
-        for (let x = 0; x < width; x++) {
-          let sumRed = 0, sumGreen = 0, sumBlue = 0, sumAlpha = 0, weightSum = 0;
-          for (let ky = -radius; ky <= radius; ky++) {
-            for (let kx = -radius; kx <= radius; kx++) {
-              const posX = Math.min(width - 1, Math.max(0, x + kx));
-              const posY = Math.min(height - 1, Math.max(0, y + ky));
-              const index = (posY * width + posX) * 4;
-              const weight = Math.exp(-(kx * kx + ky * ky) / (2 * radius * radius)) / (2 * Math.PI * radius * radius);
-              sumRed += data[index] * weight;
-              sumGreen += data[index + 1] * weight;
-              sumBlue += data[index + 2] * weight;
-              sumAlpha += data[index + 3] * weight;
-              weightSum += weight;
-            }
-          }
-          const outputIndex = (y * width + x) * 4;
-          pixels[outputIndex] = sumRed / weightSum;
-          pixels[outputIndex + 1] = sumGreen / weightSum;
-          pixels[outputIndex + 2] = sumBlue / weightSum;
-          pixels[outputIndex + 3] = sumAlpha / weightSum;
-        }
-      }
-
-      imageData.data.set(pixels);
-      context.putImageData(imageData, 0, 0);
     }
 
     onMounted(async () => {
