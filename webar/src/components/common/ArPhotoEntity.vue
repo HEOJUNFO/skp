1d2b940c2151cbe2a5e19342ddd1d94b9bb1e49b
heojunfo <kleague3@gmail.com>
2023-06-14 13:06:09 +0900

    fix: trash event handleing

--- webar/src/components/common/ArPhotoEntity.vue
+++ webar/src/components/common/ArPhotoEntity.vue
@@ -33,7 +33,8 @@
     </a-gltf-model>
 
     <a-plane v-else-if="objectType === `STICKER`" v-bind="attrs" class="clickable" gesture-handler="locationBased: true"
-      @animationcomplete="animationcomplete" renderOrder="0" @mousedown="setTrash"></a-plane>
+      @animationcomplete="animationcomplete" renderOrder="0" @mousedown="startLongPress"
+      @mouseup="cancelLongPress"></a-plane>
 
   </template>
 </template>
@@ -61,13 +62,11 @@ export default {
   emits: ['animationcomplete:object', 'timeout:object'],
   setup(props, { emit }) {
     const isTrash = ref(false);
-    // 상속받은 데이터 & 타입 & 터치이펙트 타입
+    const longPressTimer = ref(null);
+
     const { arData, arType, touchEffectType } = toRefs(props);
-    // 객체 타입
     const objectType = computed(() => arData.value.type);
-    // arData에 arType추가 (a-asset id용)
     arData.value.objectType = arType.value;
-    // 데이터에서 AR Object데이터로 변환
     const attrs = ref(getObjectAttrs(arData.value))
 
     watch(arData, (newValue) => {
@@ -78,43 +77,41 @@ export default {
       isTrash.value = !isTrash.value;
     }
 
+    const startLongPress = () => {
+      longPressTimer.value = setTimeout(() => {
+        setTrash();
+      }, 2000);
+    }
+
+    const cancelLongPress = () => {
+      clearTimeout(longPressTimer.value);
+    }
+
     const listUpdate = (arData) => {
       window.parent.stickerListDeleteItem(arData)
     }
 
-
-
-    //animation complete event
     const animationcomplete = () => {
       emit('animationcomplete:object', arData.value.itemID);
     }
 
-    //effect function for touching
     const playTouchEffect = () => {
       if (!touchEffectType.value) return;
-      //완료 팝업 삭제
-      // if (touchEffectType.value === 'NONE') {
-      //   animationcomplete();
-      // }
-      // FADEOUT효과를 gftl모델(type:6)의 경우 다른 애니메이션으로 대체
+
       if (touchEffectType.value === 'FADEOUT') {
         if (arData.value.type === "3D") {
           attrs.value.animation = getTouchAnimation('GLTFFADEOUT');
           return;
         }
       }
-      // 일반 에니메이션 효과 재생
       attrs.value.animation = getTouchAnimation(touchEffectType.value);
     }
 
-    //dom mounted 후 체크 로직
     onMounted(() => {
-      // playTime이 있는경우 playTime후 timecomplete이벤트 발생 (비디오 제외)
       if (arType.value === 'touchObject' && `VIDEO` !== objectType.value) {
         setTimeout(() => emit('timeout:object', arData.value.itemID), 3 * 1000)
       }
 
-      // 체인지 오브젝트 비디오 일 때 mounted시 재생 및 end이벤트 처리
       if (arType.value === 'touchObject' && `VIDEO` === objectType.value) {
         const videoEl = document.querySelector(attrs.value.src)
         videoEl.play();
@@ -127,7 +124,8 @@ export default {
       objectType,
       animationcomplete,
       playTouchEffect,
-      setTrash,
+      startLongPress,
+      cancelLongPress,
       isTrash,
       listUpdate
     }
