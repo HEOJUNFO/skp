37a2e6e2c928a215b04955c0ad8daa57a275aadd
heojunhyeok <junhyeok.heo@culturecon.co.kr>
2023-06-16 10:53:05 +0900

    arData decoding

--- webar/src/store/modules/url.js
+++ webar/src/store/modules/url.js
@@ -3,32 +3,47 @@
 export const url = {
   namespaced: true,
   state: () => ({
-    eventMainUrl: '/web-event/main.html',
-    eventWinningFormUrl: '/web-event/give-away.html'
+    eventMainUrl: "/web-event/main.html",
+    eventWinningFormUrl: "/web-event/give-away.html",
+    eventAppUrl: "ocbt://com.skmc.okcashbag.home_google/",
+    actionType: "WEB",
   }),
-  getters: {},
-  mutations: {},
+  getters: {
+    actionType({ actionType }) {
+      return actionType;
+    },
+  },
+  mutations: {
+    ["SET_ACTION_TYPE"](state, payload) {
+      state.actionType = payload;
+    },
+  },
   actions: {
     // main redirect
-    redirectToMain({state}, success) {
+    redirectToMain({ state }, success) {
       const container = window.top;
-      const query = (container.location.href).split('?')[1] || "";
-      if(success){
-        container.location.href = `${state.eventMainUrl}?redirect=true&${query}`;
-      }else{
-        container.location.href = `${state.eventMainUrl}?${query}`;
+      const query = container.location.href.split("?")[1] || "";
+      if (state.actionType === "OCB") {
+        container.location.href = state.eventAppUrl;
+      } else {
+        if (success) {
+          container.location.href = `${state.eventMainUrl}?redirect=true&${query}`;
+        } else {
+          container.location.href = `${state.eventMainUrl}?${query}`;
+        }
       }
     },
     // 당첨 정보 팝업
-    openWinningFormPopup({state}, {eventId, arEventWinningId, arEventWinningButtonId, eventLogWinningId}) {
-      if(!(eventId && arEventWinningId && arEventWinningButtonId)) {
+    openWinningFormPopup({ state }, { eventId, arEventWinningId, arEventWinningButtonId, eventLogWinningId }) {
+      if (!(eventId && arEventWinningId && arEventWinningButtonId)) {
         alert(`eventId, arEventWinningId, arEventWinningButtonId must be required!`);
       }
       // const query = querystring.stringify({eventId, arEventWinningId, arEventWinningButtonId, eventLogWinningId});
       // const url = `${state.eventWinningFormUrl}?${query}`;
       //sessionStorage 설정
-      const eventWinningData = {eventId, arEventWinningId, arEventWinningButtonId, eventLogWinningId};
-      sessionStorage.setItem('eventWinningData', JSON.stringify(eventWinningData));
+
+      const eventWinningData = { eventId, arEventWinningId, arEventWinningButtonId, eventLogWinningId };
+      sessionStorage.setItem("eventWinningData", JSON.stringify(eventWinningData));
       const url = `${state.eventWinningFormUrl}`;
       parent.open(url, "_blank");
     },
@@ -36,6 +51,10 @@ export const url = {
     closeWindow() {
       const container = window.top;
       container.close();
-    }
-  }
+    },
+    // 인입 경로 (WEB: 오픈웹, OCB: OCB)
+    setActionType({ commit }, param) {
+      commit("SET_ACTION_TYPE", param);
+    },
+  },
 };
--- webar/src/veiws/Landing.vue
+++ webar/src/veiws/Landing.vue
@@ -6,27 +6,27 @@
 import { computed, onMounted, ref, toRefs } from "vue";
 import { useStore } from "vuex";
 import { useRoute, useRouter } from "vue-router";
-import querystring from 'querystring'
+import querystring from "querystring";
+import cryptoUtil from "@/js/cryptoUtil";
 
 export default {
   name: "Landing",
   setup() {
-    console.log('Landing')
     const store = useStore();
     const { getters, dispatch } = store;
     const route = useRoute();
-    const { eventId, attendCode } = toRefs(route.query);
+    const { eventId, attendCode, arData } = toRefs(route.query);
+    const { aes256Decode } = cryptoUtil();
     // const router = useRouter();
 
-    const baseUrl = process.env.VUE_APP_PAGE_PATH
+    const baseUrl = process.env.VUE_APP_PAGE_PATH;
     const urls = ref([
       `${baseUrl}/basic.html#/basic`,
       `${baseUrl}/basic.html#/basic`,
       `${baseUrl}/basic.html#/basic`,
       `${baseUrl}/ar.html#/mission`,
       `${baseUrl}/basic.html#/drag-n-drop`,
-      `${baseUrl}/ar.html#/open-browser`,
-    ])
+    ]);
 
     const eventData = ref(null);
 
@@ -34,56 +34,57 @@ export default {
 
     const url = computed(() => {
       if (templateType.value) {
-        const type = ['BASIC', 'MISSION', 'BRIDGE', 'SCANNING', 'DRAG_DROP', 'FRAME']
-        let query = `?` + querystring.stringify({
-          ...(eventId?.value && { eventId: eventId?.value }),
-          ...(attendCode?.value && { attendCode: attendCode?.value }),
-          type: 'landing'
-        })
-        return urls.value[type.findIndex(item => item === templateType.value)] + query
+        const type = ["BASIC", "MISSION", "BRIDGE", "SCANNING", "DRAG_DROP"];
+        let query =
+          `?` +
+          querystring.stringify({
+            ...(eventId?.value && { eventId: eventId?.value }),
+            ...(attendCode?.value && { attendCode: attendCode?.value }),
+            type: "landing",
+          });
+        return urls.value[type.findIndex((item) => item === templateType.value)] + query;
       }
-      return null
-    })
+      return null;
+    });
 
     // 페이지를 벗어나기 전에 localstorage제거
-    window.addEventListener('unload', () => {
-      window.localStorage.removeItem('event_validation')
-    })
+    window.addEventListener("unload", () => {
+      window.localStorage.removeItem("event_validation");
+    });
 
     const loadComplete = () => {
       if (url.value !== null) {
         // iframe loading complete
         // TODO ifame이 로딩되기 전에 화면 가림 처리?
       }
-    }
+    };
 
     const webEventGetTraceNo = function () {
       const timezoneOffset = new Date().getTimezoneOffset() * 60000;
       const timezoneDate = new Date(Date.now() - timezoneOffset);
-      return (timezoneDate).toISOString().slice(0, 19).replace(/[-T:]/g, "") + (Math.random() + "").slice(2, 5);
+      return timezoneDate.toISOString().slice(0, 19).replace(/[-T:]/g, "") + (Math.random() + "").slice(2, 5);
     };
 
     onMounted(async () => {
-      const eventValidation = window.localStorage.getItem('event_validation')
-      const isLocal = window.location.port !== ''
-      console.log('isLocal', isLocal)
+      const eventValidation = arData?.value;
+      // const eventValidation = window.localStorage.getItem('event_validation')
+      const isLocal = window.location.port !== "";
 
       if (!isLocal && !eventId?.value) {
         // TODO query 없음! 리턴 처리 or 창닫기 로직 백엔드와 개발해서 추가
         // console.log('파라미터 없음! 리턴 처리 or 창닫기')
-        await dispatch('url/closeWindow')
+        await dispatch("url/closeWindow");
         // return
       }
 
       if (!isLocal && !eventValidation) {
         // console.log('정상적인 접근이 아님! 리턴 처리 or 창닫기')
-        await dispatch('url/redirectToMain')
+        await dispatch("url/redirectToMain");
       }
-
       if (isLocal && !eventId?.value) {
-        alert('[LOCAL]: eventId가 없습니다!');
+        alert("[LOCAL]: eventId가 없습니다!");
         const router = useRouter();
-        await router.push({ name: 'Landing', query: { eventId: '' } })
+        await router.push({ name: "Landing", query: { eventId: "" } });
         return;
       }
 
@@ -97,24 +98,28 @@ export default {
           ...(latitude && { latitude }),
           ...(longitude && { longitude }),
           ...(attendCode?.value && { attendCode: attendCode?.value }),
-        }
-        await dispatch('eventData/getEventData', params)
+        };
+        await dispatch("eventData/getEventData", params);
         // store에서 데이터 파싱
-        eventData.value = getters['eventData/eventData'];
+        eventData.value = getters["eventData/eventData"];
         // 세션스토리지에 json데이터 저장
-        sessionStorage.setItem('skWebArJson', JSON.stringify(eventData.value));
+
+        sessionStorage.setItem("skWebArJson", JSON.stringify(eventData.value));
+
+        const eventValidationData = JSON.parse(aes256Decode(eventValidation));
+        await dispatch("url/setActionType", eventValidationData.activeType);
       } catch (err) {
-        await dispatch('url/redirectToMain')
+        await dispatch("url/redirectToMain");
         // return
       }
-    })
+    });
 
     return {
       url,
       loadComplete,
     };
-  }
-}
+  },
+};
 </script>
 
 <style scoped></style>
