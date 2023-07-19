5f8b4e6d5fd1d4a9f6e389dbddd357da78bf5aed
heojunhyeok <junhyeok.heo@culturecon.co.kr>
2023-07-19 13:01:12 +0900

    fix: orientation

--- webar/src/components/common/Camera.vue
+++ webar/src/components/common/Camera.vue
@@ -37,12 +37,8 @@ export default {
       cameraSettings.facingMode = cameraSettings.facingMode === 'user' ? 'environment' : 'user';
       cameraSettings.isFlipped = !cameraSettings.isFlipped;
 
-      // if (window.AFRAME.scenes.length > 0) {
-      //   window.AFRAME.scenes[0].sceneEl.systems['mindar-face-system'].changeCamera(cameraSettings.facingMode);
-      // }
-
-      if (window.parent.AFRAME.scenes.length > 0) {
-        window.parent.AFRAME.scenes[0].sceneEl.systems['mindar-face-system'].changeCamera(cameraSettings.facingMode);
+      if (window.AFRAME.scenes.length > 0) {
+        window.AFRAME.scenes[0].sceneEl.systems['mindar-face-system'].changeCamera(cameraSettings.facingMode);
       }
 
       try {
